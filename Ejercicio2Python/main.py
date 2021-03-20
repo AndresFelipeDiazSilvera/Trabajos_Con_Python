26dc36973529ce8e286fd46d02c4f987ad90b84c
Andres Felipe Diaz Silvera <andres.diazs@usantoto.edu.co>
2021-03-19 20:17:30 -0500

    Second Exercise / Arithmetic Operation
    Ejercico de operacion aritmética desarrollada en python

---
+++ Ejercicio2Python/main.py
@@ -0,0 +1,11 @@
+# AUTHOR: Andres Felipe Diaz Silvera
+# DATE: 19/03/2021
+
+#Ejercicio 2
+
+a = float(input("Digite A:"))
+b = float(input("Digite B:"))
+
+c=((3+5*8)<3 and ((-6/3*4)+2<2)) or (a>b)
+
+print(f"El resultado es {c}")
