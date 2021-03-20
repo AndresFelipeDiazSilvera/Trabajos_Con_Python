0c8572be0a8b6fdfcff69266c6ad88120044b9eb
Andres Felipe Diaz Silvera <andres.diazs@usantoto.edu.co>
2021-03-19 21:04:25 -0500

    Fourth Exercise /  Swap the value of 2 variables
    Exercise where a program is carried out where variables are exchanged

---
+++ Ejercicio4/main.py
@@ -0,0 +1,14 @@
+#AUTHOR: Andres Felipe Diaz Silvera
+#Date: 19/03/2021
+
+#Ejercicio 4
+
+a = input("a -> ")
+b = input("b -> ")
+
+a , b = b , a
+
+print(f" El nuevo valor de a es: {a}")
+print(f" El nuevo valor de b es: {b}")
+
+
